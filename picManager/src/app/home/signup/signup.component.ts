9d6a77aa59a051a78710ecd2b3d1e9123a8588a0
C1313769 Diego Fernandes Paz <c1313769@interno.bb.com.br>
2022-06-18 17:50:51 -0300

    first commit

---
+++ picManager/src/app/home/signup/signup.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { lowerCaseValidator } from 'src/app/shared/validator/lower-case.validator';
+import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
+
+@Component({
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignUpComponent implements OnInit {
+
+  signupForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userNotTakenValidatorService: UserNotTakenValidatorService) {}
+
+  ngOnInit(): void {
+      this.signupForm = this.formBuilder.group({
+          email: ['',
+              [
+                  Validators.required,
+                  Validators.email
+              ]
+          ],
+          fullName: ['',
+              [
+                  Validators.required,
+                  Validators.minLength(2),
+                  Validators.maxLength(40)
+              ]
+          ],
+          userName: ['',
+              [
+                  Validators.required,
+                  lowerCaseValidator,
+                  Validators.minLength(2),
+                  Validators.maxLength(30)
+              ],
+              this.userNotTakenValidatorService.checkUserNameTaken()
+          ],
+          password: ['',
+              [
+                  Validators.required,
+                  Validators.minLength(8),
+                  Validators.maxLength(14)
+              ]
+          ]
+      });
+  }
+}
